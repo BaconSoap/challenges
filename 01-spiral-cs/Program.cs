48799c52d5135ea04b2b3ac8bf6cb4bb331c5a9d
Andrew Varnerin <andrew@varnerin.info>
2015-05-31 14:23:14 -0400

    initial spiral structure

--- 01-spiral-cs/Program.cs
+++ 01-spiral-cs/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace spiral
+﻿using System;
+
+namespace spiral
 
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var size = new Tuple<int, int>(30, 20);
+
+            var spiral = MakeSpiral(size.Item1, size.Item2);
+
+            Print(spiral);
+        }
+
+        private static void Print(SafeGrid<string> spiral)
+        {
+            
+        }
+
+        private static SafeGrid<string> MakeSpiral(int rows, int cols)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SafeWrite(int x, int y)
+        {
+            
         }
     }
 }
---
+++ 01-spiral-cs/SafeGrid.cs
@@ -0,0 +1,41 @@
+﻿namespace spiral
+{
+    public class SafeGrid<T>
+    {
+        private readonly int _width;
+        private readonly int _height;
+        private readonly T[,] _cells;
+
+        public T this[int row, int col]
+        {
+            get
+            {
+                if (IsIndexOutOfBounds(row, col))
+                {
+                    return default(T);
+                }
+                return _cells[row, col];
+            }
+            set
+            {
+                if (IsIndexOutOfBounds(row, col))
+                {
+                    return;
+                }
+                _cells[row, col] = value;
+            }
+        }
+
+        private bool IsIndexOutOfBounds(int row, int col)
+        {
+            return row >= _width || row < 0 || col >= _height || col < 0;
+        }
+
+        public SafeGrid(int height, int width)
+        {
+            _width = width;
+            _height = height;
+            _cells = new T[height, width];
+        }
+    }
+}
