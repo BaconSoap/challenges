fd209e808caaf6889b8bcc05b51e4c4a00602d36
Andrew Varnerin <andrew@varnerin.info>
2015-05-31 14:35:57 -0400

    working spiral

--- 01-spiral-cs/Program.cs
+++ 01-spiral-cs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace spiral
 
@@ -7,26 +8,48 @@ namespace spiral
     {
         static void Main(string[] args)
         {
-            var size = new Tuple<int, int>(30, 20);
+            var size = new Tuple<int, int>(30, 70);
 
             var spiral = MakeSpiral(size.Item1, size.Item2);
 
             Print(spiral);
+            Console.ReadLine();
         }
 
         private static void Print(SafeGrid<string> spiral)
         {
-            
+            var sb = new StringBuilder();
+            for (var row = 0; row < spiral.Height; row++)
+            {
+                for (var col = 0; col < spiral.Width; col++)
+                {
+                    var value = spiral[row, col];
+                    sb.Append(string.IsNullOrEmpty(value)? " ": value);
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
         }
 
         private static SafeGrid<string> MakeSpiral(int rows, int cols)
         {
-            throw new NotImplementedException();
-        }
+            var step = (2*Math.PI)/100.0d;
+            var rMax = 25*2*Math.PI;
+            var halfX = cols/2;
+            var halfY = rows/2;
 
-        private static void SafeWrite(int x, int y)
-        {
-            
+            var grid = new SafeGrid<string>(rows, cols);
+
+            for (var r = 0d; r < rMax; r += step)
+            {
+                var x = Math.Sin(r) * r;
+                var y = Math.Cos(r) * r;
+                var col = halfX + (int)Math.Round(x);
+                var row = halfY + (int)Math.Round(y);
+                grid[row, col] = ".";
+            }
+
+            return grid;
         }
     }
 }
--- 01-spiral-cs/SafeGrid.cs
+++ 01-spiral-cs/SafeGrid.cs
@@ -2,8 +2,10 @@
 {
     public class SafeGrid<T>
     {
-        private readonly int _width;
-        private readonly int _height;
+        public int Width { get; private set; }
+        
+        public int Height { get; private set; }
+
         private readonly T[,] _cells;
 
         public T this[int row, int col]
@@ -28,13 +30,13 @@
 
         private bool IsIndexOutOfBounds(int row, int col)
         {
-            return row >= _width || row < 0 || col >= _height || col < 0;
+            return row >= Height || row < 0 || col >= Width || col < 0;
         }
 
         public SafeGrid(int height, int width)
         {
-            _width = width;
-            _height = height;
+            Width = width;
+            Height = height;
             _cells = new T[height, width];
         }
     }
